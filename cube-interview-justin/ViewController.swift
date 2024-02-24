71c49d9f942edf367963982d054fb851609cbc69
Justin <justinlove0987@gmail.com>
2024-02-24 17:12:30 +0800

    Initial Commit

---
+++ cube-interview-justin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cube-interview-justin
+//
+//  Created by 曾柏楊 on 2024/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
